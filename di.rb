a0b3af95b14ff755bbc373c32b319e1ce3801ef7
Madson Cunha <madson.cunha@buscapecompany.com>
2014-10-07 17:22:26 -0300

    Command "di" are running

--- di.rb
+++ di.rb
@@ -1,10 +1,36 @@
 require 'rubygems'
-require 'highline'
+require 'highline/import'
 
 output = `git status --porcelain | grep "^[ ]\\?M" | sed s/^...//`
 
 files = output.split("\n")
 
-puts files
+def show_diff_for(filename)
+	system("git diff " + filename)
+	exit 0
+end
 
+def show_menu
+	choose do |menu|
+		menu.prompt = "Please, select one file to see the diff "
+		limit = files.size
+		
+		for i in 0...limit
+			filename = files[i]
+
+			menu.choice(filename) do
+				show_diff_for(filename)
+			end
+		end
+	end
+end
+
+if files.size == 0
+	puts "There are not modified files"
+	exit 0
+elsif files.size == 1
+	show_diff_for(files.first)
+else
+	show_menu
+end
 
