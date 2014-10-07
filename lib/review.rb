f0c78ff9812826f45b10af1d3ffe8ae1c1788a27
Madson Cunha <madson.cunha@buscapecompany.com>
2014-10-07 20:25:14 -0300

    Added ability to add file to stage

--- lib/review.rb
+++ lib/review.rb
@@ -4,11 +4,29 @@ require 'highline/import'
 module Review
 	class << self
 
-		OUTPUT = `git status --porcelain | grep "^[ ]\\?M" | sed s/^...//`
-		FILES = OUTPUT.split("\n")
+		def files
+			output = `git status --porcelain | grep "^\\(.\\)\\+M" | sed s/^...//`
+			output.split("\n")
+		end
 
 		def show_diff_for(filename)
 			cmd = "git diff " + filename
+			system(cmd)
+			ask_to_add filename
+		end
+
+		def ask_to_add(filename)
+			choose do |cmd|
+				cmd.index = :none
+				cmd.prompt = "Add to stage? "
+				cmd.layout = :one_line
+				cmd.choice("yes") { add_to_stage filename  }
+				cmd.choice("no")
+			end
+		end
+
+		def add_to_stage(filename)
+			cmd = "git add " + filename
 			system(cmd)
 		end
 
@@ -24,11 +42,12 @@ module Review
 
 				menu.choice("<%= color('Stop stage review', :system_option) %>") { exit 1 }
 
-				for i in 0...FILES.size
-					filename = FILES[i]
+				for i in 0...files.size
+					filename = files[i]
+					option_string = "<%= color('#{filename}', :simple_option)  %>"
 
-					menu.choice("<%= color('#{filename}', :simple_option)  %>") do
-						show_diff_for(filename)
+					menu.choice(option_string) do
+						show_diff_for filename
 					end
 				end
 			end
@@ -37,14 +56,17 @@ module Review
 		end
 
 		def run
-			if FILES.size == 0
-				puts "There are no modified files"
+			if files.size == 0
+				puts "There are no unstaged files"
 				exit 0
-			elsif FILES.size == 1
-				show_diff_for(FILES.first)
+			elsif files.size == 1
+				show_diff_for(files.first)
 			else
 				show_menu
 			end
+
+			system("git status -s")
+			exit 1
 		end
 
 	end # class
